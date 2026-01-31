c55a094d00bc786c9d2c180f2e8f4f8441fb59e0
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2026-01-31 15:14:40 +0100

    Feat : max_range for draggable

--- LightShow/Assets/Prefabs/MouseDrag.cs
+++ LightShow/Assets/Prefabs/MouseDrag.cs
@@ -2,18 +2,45 @@
 // Posted by Ghasem, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-01-31, License - CC BY-SA 4.0
 
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(BoxCollider2D))]
 
 public class MouseDrag : MonoBehaviour {
     [SerializeField] private bool isDragging = false;
+    [SerializeField] private float maxUp, maxDown = 0.0f;
+    private float maxY, minY;
+    private Vector3 spawnPos;
+
+    void Start()
+    {
+        spawnPos = transform.position;
+        maxY = spawnPos.y + maxUp;
+        minY = spawnPos.y - maxDown;
+    }
 
     void Update()
     {
         if(isDragging)
         {
-            transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+            float newY = transform.position.y;
+            if (mouseY <= maxY && mouseY >= minY)
+            {
+                newY = mouseY;
+            }
+            else if (mouseY > maxY)
+            {
+                newY = maxY;
+            }
+            else if (mouseY < minY)
+            {
+                newY = minY;
+            }
+            Vector3 newPos = new Vector3(transform.position.x, newY, transform.position.z);
+            transform.position = newPos;
         }
     }
 
